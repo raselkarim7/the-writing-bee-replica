c363188a8416f9f27f947d09df7804612bc92409
raselkarim7 <rtc.raselkarim7@gmail.com>
2021-01-24 21:09:18 +0600

    header and hero section complete of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import BoomWriterLogo from '../assets/boomwriter-logo.png'
+import HeroGlobImg from '../assets/hero-glob.png'; 
+import HeroTriangleImg from '../assets/hero-triangle.png'; 
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Js</h1>
-    </div>
+      <section id="home-info" > 
+      <div className="hero-container">
+        <div>
+          <h1 className="hero-heading">
+            <Link className="hero-heading-link" to="bee/2019/spoken_word.html">
+              <span className="hero-scroll">The 2020</span><br/>
+              WRITING BEE
+            </Link>
+          </h1>
+          <p className=" mx-auto hero-subheading  mb-3">Because all kids love writing stories!</p>
+          <p className=" mx-auto hero-subheading mt-1 mt-md-5 col-12 mb-0 h5">Powered by:</p>
+          <img className="img-fluid hero-bw-logo mx-auto" src={BoomWriterLogo} alt="Boomwriter bwm rgb rev" />
+        </div>
+        <img className="hero-img position-relative" src={HeroGlobImg} />
+        <img className="hero-triangle" src={HeroTriangleImg} />
+      </div>
+    </section>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useRef } from 'react';
+import { Link } from 'react-router-dom'
+
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const navRef = useRef(null)
+  const handleBurgerClick = () => {
+    let x = navRef.current;
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
   return (
     <nav>
-        <h3> Navbar </h3>
+      <div className="topnav" ref={navRef} id="myTopnav">
+        <div> 
+          <Link to="/" className="active">
+            <img src={logo} />
+          </Link>
+          <Link to="/">Home</Link>
+          <Link to="/past-writing-bees">Past Writing Bees</Link>
+        </div>
+        <div to="javascript:void(0);" className="icon" onClick={handleBurgerClick}>
+          <i className="fa fa-bars"></i>
+        </div>
+      </div>
     </nav>
   );
 };
